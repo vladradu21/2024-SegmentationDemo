1f631da695810923ab60a64016ec884437789103
vladradu <mevladradu@gmail.com>
2024-03-20 12:46:03 +0200

    unet: resize_data.py

--- models/resize_data.py
+++ models/resize_data.py
@@ -8,7 +8,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
-SIZE = (512, 512)
+SIZE = (1024, 1024)
 
 dataset_configurations = {
     "RIM_ONE": {
